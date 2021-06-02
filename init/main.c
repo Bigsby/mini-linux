ab869c4cb561e900d5ada8426b6f8908a67ff000
Bruno Bernardo <bigsby@live.com>
2021-06-02 16:36:41 +0100

    list root directory

--- init/main.c
+++ init/main.c
@@ -3,10 +3,59 @@
 #include <string.h>
 #include <linux/reboot.h>
 #include <sys/syscall.h>
+#include <sys/sysinfo.h>
+#include <linux/fcntl.h>
+#include <dirent.h>
 
 void quit()
 {
     syscall(SYS_reboot, LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_POWER_OFF, 0);
+}
+
+char dTypeDisplay(char d_type)
+{
+    switch (d_type)
+    {
+        case DT_BLK:
+            return 'b';
+        case DT_CHR:
+            return 'c';
+        case DT_DIR:
+            return 'd';
+        case DT_FIFO:
+            return 'f';
+        case DT_LNK:
+            return 'l';
+        case DT_REG:
+            return '-';
+        case DT_SOCK:
+            return 's';
+        case DT_UNKNOWN:
+            return 'u';
+        default:
+            return 'U';
+    }
+}
+
+void printFileSystem()
+{
+    struct dirent *item;
+    DIR *pDir;
+    pDir = opendir(".");
+    while ((item = readdir(pDir)) != NULL)
+        printf("%c %s\n", dTypeDisplay(item->d_type), item->d_name);
+
+    closedir(pDir);
+}
+
+void printSysInfo()
+{
+    struct sysinfo info;
+    syscall(SYS_sysinfo, &info);
+    printf("Uptime:\t\t%ld\tseconds\n", info.uptime);
+    printf("Total RAM:\t%ld\tbytes\n", info.totalram);
+    printf("Free RAM:\t%ld\tbytes\n", info.freeram);
+    printf("Proc count:\t%d\n", info.procs);
 }
 
 void printSplash()
@@ -28,6 +77,8 @@ void printUsage()
 {
     printf("\033[1;37mBigsby Linux v0.0.1\033[0m\n\n");
     printf("Available commads:\n");
+    printf("\tinfo\t\tdisplay system information\n");
+    printf("\tls\t\tlist filesystem content");
     printf("\tclear\t\tclear screen\n");
     printf("\thelp\t\tprint this help\n");
     printf("\tquit\t\texit system\n");
@@ -47,6 +98,10 @@ int main()
         fgets(command, 50, stdin);
         if (strncmp(command, "quit", 4) == 0)
             quit();
+        else if (strncmp(command, "info", 4) == 0)
+            printSysInfo();
+        else if (strncmp(command, "ls", 2) == 0)
+            printFileSystem();
         else if (strncmp(command, "help", 4) == 0)
             printUsage();
         else if (strncmp(command, "clear", 4) == 0)
